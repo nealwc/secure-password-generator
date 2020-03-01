4fd1362b5a79725bf0e95ee9283f956fbcee857d
Carter Neal <55357370+nealwc@users.noreply.github.com>
2020-02-29 20:05:09 -0500

    Rebuilt inputs and validations

--- script.js
+++ script.js
@@ -1,59 +1,59 @@
-// while loop to prompt for correct length until the correct length is given
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l" ,"m" ,"n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numeric = "0123456789";
+var special = "~!@#$%^&*()_+-=[]\{}|;':,./<>?";
 
-var i = 0
+var password = [];
 
-while (i < 1) {
+function generatePassword() {
     var passwordLength = prompt("Please choose a password length between 8 and 128 characters");
-    if (passwordLength >= 8 && passwordLength <= 128) {
-        console.log(passwordLength);
-        alert("Your password meets all the required criteria");
-        i++;
+    console.log(passwordLength);
+    if (passwordLength < 8 || passwordLength >= 128) {
+        alert("Your password must be between 8 and 128 characters, please chose again.");
+        return; // exits func
     }
-    else {
-        alert("Your password please chose again.")
-    }
-}
-
-alert("Your password must include at least a lowercase, uppercase, numeric or special characters.")
-
-var i = 0
-
-while (i < 1) {
-    
     var confirmLowercase = confirm("Do you want to include lowercase characters in your password?");
     var confirmUppercase = confirm("Do you want to include uppercase characters in your password?");
     var confirmNumeric = confirm("Do you want to include numeric characters in your password?");
     var confirmSpecial = confirm("Do you want to include special characters in your password?");
-    if (confirmLowercase === true || confirmUppercase === true || confirmNumeric === true || confirmSpecial === true) {
-        console.log(confirmLowercase);
-        console.log(confirmUppercase);
-        console.log(confirmNumeric);
-        console.log(confirmSpecial);
-        alert("Good jorb");
-        i++;
+    console.log(confirmLowercase);
+    console.log(confirmUppercase);
+    console.log(confirmNumeric);
+    console.log(confirmSpecial);
+    if (confirmLowercase === false && confirmUppercase === false && confirmNumeric === false && confirmSpecial === false) {
+        alert("Please include at least one of the characters?");
+        return;
     }
-    else {
-        alert("Choose again.")
-    }
+if (confirmLowercase === true) {
+    password.push(lowercase);
+    console.log(password);
 }
+if (confirmUppercase === true) {
+    password.push(uppercase);
+    console.log(password);
+}
+if (confirmNumeric === true) {
+    password.push(numeric);
+    console.log(password);
+}
+if (confirmSpecial === true) {
+    password.push(special)
+    console.log(password);
+}
+for (i = 0; i < passwordLength; i++) {
+    // var a = ['a', 'b', 'c', 'd', 'e', 'f'];
+    var newPassword = password[Math.floor(password.length * Math.random())];
+    console.log(newPassword);
+}
+}
+generatePassword();
 
-var lowercase = ["abcdefghijklmnopqrstuvwxyk"];
-var test = lowercase.split("");
-for(i = 0; i < 6; i++) {
-    console.log(test);
-}
-var uppercase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
-var numberic = ["0123456789"]
-var special = ["~!@#$%^&*()_+-=[]\{}|;':,./<>?"]
 
-for (i = 0; i < passwordLength; i++) {        
-    
-    password = randomCharString.charAt(Math.floor(Math.random() * Math.floor((randomCharString.length) - 1)));        
-}
-    
-function geratePassword() {
+// Run rest of code under here
+// confirm types
+// for loop to assign random value
+// display final result on page
 
-}
 
 // <!-- GIVEN I need a new, secure password
 // WHEN I click the button to generate a password
@@ -70,5 +70,3 @@ function geratePassword() {
 // THEN a password is generated that matches the selected criteria
 // WHEN the password is generated
 // THEN the password is either displayed in an alert or written to the page -->
-
-
---
+++ test.js
@@ -0,0 +1,16 @@
+for (var i = 0; i < 10; i++) {
+    // Generate a random number between 1 and 10
+    // Math.floor will round down, meaning we would get a number between 0 and 9, so we'll always add 1 to bump it up.
+    // var num = Math.floor(Math.random() * 10) + 1;
+    // var char = charAt(Math.floor(Math.random());
+
+    // // Display in console
+    // // console.log(num);
+    // console.log(char);
+    
+    var a = ['a', 'b', 'c', 'd', 'e', 'f'];
+    var randomValue = a[Math.floor(a.length * Math.random())];
+console.log(randomValue);
+
+  }
+
