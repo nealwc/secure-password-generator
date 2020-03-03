81c615e946ddfa00aa189891544cffbcf712018c
Carter Neal <55357370+nealwc@users.noreply.github.com>
2020-03-02 21:18:34 -0500

    updated readme, including preview photo, cleaned up html

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # secure-password-generator
+
+Secure password generator generates a random password given user inputs. User determines password length between 8 and 128 characters and choses at least one type of character: lowercase, uppercase, numeric and/or special. Javascript program validates input, generates a random password and displays on the screen. User can generate a new password without leaving the webpage.
+
+## UI of Program
+
+![preview of interface](./assets/password-generator-preview.png)
--- script.js
+++ script.js
@@ -8,46 +8,41 @@ var passwordInputs = [];
 var newPassword = "";
 
 buttonEl.addEventListener("click", function generatePassword() {
-    var passwordLength = prompt("Please choose a password length between 8 and 128 characters");
-    console.log(passwordLength);
+    var passwordLength = prompt("Please choose a password length between 8 and 128 characters"); // determines size of password and validates
     if (passwordLength < 8 || passwordLength >= 128) {
         alert("Your password must be between 8 and 128 characters, please chose again.");
-        return; // exits func
+        return;
     }
+    // 
     var confirmLowercase = confirm("Do you want to include lowercase characters in your password?");
     var confirmUppercase = confirm("Do you want to include uppercase characters in your password?");
     var confirmNumeric = confirm("Do you want to include numeric characters in your password?");
     var confirmSpecial = confirm("Do you want to include special characters in your password?");
-    console.log(confirmLowercase);
-    console.log(confirmUppercase);
-    console.log(confirmNumeric);
-    console.log(confirmSpecial);
+
     if (confirmLowercase === false && confirmUppercase === false && confirmNumeric === false && confirmSpecial === false) {
         alert("Please include at least one of the characters");
         return;
     }
 if (confirmLowercase === true) {
     passwordInputs.push(lowercase);
-    console.log(passwordInputs);
+    // checks if user wants to include lowercase characters and adds to passwordInputs variable
 }
 if (confirmUppercase === true) {
     passwordInputs.push(uppercase);
-    console.log(passwordInputs);
+    // checks if user wants to include uppercase characters and adds to passwordInputs variable
 }
 if (confirmNumeric === true) {
     passwordInputs.push(numeric);
-    console.log(passwordInputs);
+    // checks if user wants to include numeric characters and adds to passwordInputs variable
 }
 if (confirmSpecial === true) {
     passwordInputs.push(special)
-    console.log(passwordInputs);
+    // checks if user wants to include special characters and adds to passwordInputs variable
 }
 
-for (i = 0; i < passwordInputs.length; i++) {
+for (i = 0; i < passwordInputs.length; i++) { // uses password length to run for loop
     if (i === 0) {
         for (j = 0; j < ( passwordLength - passwordInputs.length + 1 ); j++) {
-            console.debug("lengths", passwordLength, passwordInputs.length);
-
             var character = passwordInputs[0].charAt(Math.floor(Math.random() * passwordInputs[0].length)); 
             newPassword = newPassword.concat(character);       
         }
@@ -56,32 +51,9 @@ for (i = 0; i < passwordInputs.length; i++) {
         newPassword = newPassword.concat(character);
     }
 }
-console.log("newPassword", newPassword);
-userPassword.textContent = newPassword
+userPassword.textContent = newPassword 
 })
 
 generatePassword();
 
-userPassword.appendChild(newPassword);
-
-// Run rest of code under here
-// confirm types
-// for loop to assign random value
-// display final result on page
-
-
-// <!-- GIVEN I need a new, secure password
-// WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-// WHEN prompted for character types to include in the password
-// THEN I choose lowercase, uppercase, numeric, and/or special characters
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page -->
+userPassword.appendChild(newPassword); // updates html with generated password
